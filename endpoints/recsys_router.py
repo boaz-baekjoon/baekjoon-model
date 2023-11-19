ccb9524119bb16ab669af2f6fa0264f2e71b21bc
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2023-11-19 17:01:12 +0900

    [Refactor] #2 enhance readability
    - use ast to eval string list
    - added logger to the endpoint

--- endpoints/recsys_router.py
+++ endpoints/recsys_router.py
@@ -1,17 +1,18 @@
-import json
 from fastapi import APIRouter, Depends
-from typing import List
 
 from database import *
 from schema.schema import UserIDRequest
-
 from init_model import infer_model, args
+
+from utils.log import make_logger
+from ast import literal_eval
+
+user_id_logger = make_logger("user_id_logger")
 
 router = APIRouter(
     prefix="/baekjun"
 )
 
-# load model
 @router.post("/user_id")
 async def get_user(input : UserIDRequest):
     userid_problem_list = dict()
@@ -19,14 +20,26 @@ async def get_user(input : UserIDRequest):
         try: 
             user_id_int = user_id_dict[user_id]
             user_problem_list = user_problem_table[user_problem_table.user_id_int == user_id_int]
-            user_seq = [int(x[1:-1])for x in user_problem_list.iloc[0,1].strip("[]").split(", ")]
-            result = infer_model.predict_for_user_sq(user_seq, input.problem_num, problem_list ,args)
-            userid_problem_list[user_id] = result 
-        except:
-            # todo : solve.ac 에서 유저 정보 가져오기
+            user_seq = literal_eval(user_problem_list.iloc[0, 1])
+            user_tier = user_detail[user_detail.user_id == user_id].iloc[0, 1]
+            
+            problem_list = problem_detail[(problem_detail.problem_level >= user_tier - 3) & (problem_detail.problem_level >= user_tier + 3)]
+            result = infer_model.predict_for_user_sq(
+                sequence=user_seq, 
+                item_num=input.problem_num, 
+                problem_list=problem_list ,
+                args=args
+            )
+            userid_problem_list[user_id] = list(map(int, result))
+            user_id_logger.info({f"'type': 'recsys', 'user_id': '{user_id}', 'problem_list': {result}"})
+            
+        except: 
+            problem_list = problem_detail[(problem_detail.problem_level <= 15)][['problem_id']]
             result = list(problem_list.sample(n=input.problem_num).to_dict(orient='records'))
             userid_problem_list[user_id] = [problem['problem_id'] for problem in result]
             
+            user_id_logger.info({f"'type': 'except', 'user_id': '{user_id}', 'problem_list': {userid_problem_list[user_id]}"})
+
     return userid_problem_list
 
 @router.get("/similar_id")
