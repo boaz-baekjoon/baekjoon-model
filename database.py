68d379a39c9961b324ffc56c7ff8d4d9ab8790de
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2023-11-19 17:00:07 +0900

    [Feat] #2 include new fields to db
    - problem detail (tier infos)
    - user detail (tier infos)

--- database.py
+++ database.py
@@ -5,3 +5,5 @@ problem_list = pd.read_csv("data/preproc_data/problem_id.csv")
 user_problem_table = pd.read_csv('data/preproc_data/baekjoon.csv')
 user_id_map = pd.read_csv("data/preproc_data/user_id_int_map.csv")
 user_id_dict = user_id_map.set_index('user_id').to_dict()['user_id_int']
+problem_detail = pd.read_csv("data/preproc_data/problem_detail.csv")[['problem_id', 'problem_level']]
+user_detail = pd.read_csv("data/preproc_data/users_detail.csv")[['user_id','user_tier']]
