0cb6f78092001d94b7dbaa810b87bdbaea496153
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2023-11-24 18:57:40 +0900

    [Fix] #2 Fix logger
    - change FileHandler to TimedRotatingFileHandler

--- utils/log.py
+++ utils/log.py
@@ -3,26 +3,30 @@ import logstash
 from datetime import datetime
 
 def make_logger(name=None):
-    logger = logging.getLogger(name)
+    logger = logging.getLogger(name, )
  
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
  
     formatter = logging.Formatter("[%(asctime)s] %(name)s - %(levelname)s - %(message)s")
      
     console = logging.StreamHandler()
-    file_handler = logging.FileHandler(filename="logs/{:%Y-%m-%d}.log".format(datetime.now()))
-     
     console.setLevel(logging.INFO)
-    file_handler.setLevel(logging.DEBUG)
- 
     console.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
- 
+    
+    # file_handler = logging.FileHandler(filename="logs/{:%Y-%m-%d}.log".format(datetime.now()))
+    # file_handler.setLevel(logging.DEBUG)
+    # file_handler.setFormatter(formatter)
+    
+    timedfilehandler = logging.handlers.TimedRotatingFileHandler(filename='logs/user_id_recsys.log'.format(datetime.now()), when='midnight', interval=1, encoding='utf-8')
+    timedfilehandler.setFormatter(formatter)   
+    timedfilehandler.suffix = "%Y-%m-%d"
+    
+    # logger.addHandler(file_handler)
     logger.addHandler(console)
-    logger.addHandler(file_handler)
+    logger.addHandler(timedfilehandler)
  
-    stash = logstash.TCPLogstashHandler('localhost',5000,version=1)
-    stash.setFormatter(formatter)
-    logger.addHandler(stash)
+    # stash = logstash.TCPLogstashHandler('localhost',5000,version=1)
+    # stash.setFormatter(formatter)
+    # logger.addHandler(stash)
  
     return logger
